e994b4b497df94cc620ddb1294f1b1bd67d810c1
Amar Goyal <goyalamars18@gmail.com>
2026-09-20 21:28:46 +0000

    feat(preload): read and update app settings

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,6 +2,7 @@ import { contextBridge, ipcRenderer, webUtils } from 'electron';
 import type { ApprovalDecision, HitRect, Snapshot } from '../shared/types';
 import type { CompanionPreferences, CompanionSnapshot, CompanionView, SmartShuffleAnswer, SpotifyCommand } from '../shared/companion';
 import type { UpdateInfo, UpdateResponse } from '../shared/updates';
+import type { AppSettingsPatch } from '../shared/settings';
 
 function subscribe<T>(channel: string, cb: (value: T) => void): () => void {
   const listener = (_event: Electron.IpcRendererEvent, value: T) => cb(value);
@@ -50,5 +51,7 @@ contextBridge.exposeInMainWorld('notchlight', {
   clearClipboard: (includePinned: boolean) => ipcRenderer.invoke('clipboard:clear', includePinned),
   pauseClipboard: (paused: boolean) => ipcRenderer.invoke('clipboard:pause', paused),
   onUpdate: (cb: (info: UpdateInfo) => void) => subscribe('update', cb),
-  respondToUpdate: (response: UpdateResponse) => ipcRenderer.send('update:respond', response)
+  respondToUpdate: (response: UpdateResponse) => ipcRenderer.send('update:respond', response),
+  getAppSettings: () => ipcRenderer.invoke('app:settings'),
+  updateAppSettings: (patch: AppSettingsPatch) => ipcRenderer.invoke('app:settings:update', patch)
 });
